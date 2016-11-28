09f9445d30a4e7a83860df6d521fe6b78fbb3451
Liwei Lin <lwlin7@gmail.com>
2016-11-28 23:20:37 +0800

    updates

--- lin_robot.py
+++ lin_robot.py
@@ -1,14 +1,21 @@
-#coding=utf-8
+# coding=utf-8
 import itchat
+import time
+
+
+def printTime():
+    ISOTIMEFORMAT = 'TIME = %Y-%m-%d %X'
+    print time.strftime(ISOTIMEFORMAT, time.gmtime(time.time() + 8 * 3600))
 
 
 @itchat.msg_register(['Note'])
 def text_reply(msg):
     print '== msg_register(Note) begins ====='
+    printTime()
     print msg
-    print '=================================='
+    # print '=================================='
     itchat.add_friend(userName=msg['FromUserName'], status=2)
-    itchat.add_friend(userName=msg['FromUserName'], status=3)
+    # itchat.add_friend(userName=msg['FromUserName'], status=3)
     print '== msg_register(Note) ends ======='
     return u"Hello !"
 
@@ -16,31 +23,40 @@ def text_reply(msg):
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
     print '== msg_register(TEXT) begins ====='
+    printTime()
     print msg
     print '== msg_register(TEXT) ends ======='
     response = ''
     # print 'Text = '
     # print msg['Text']
-    itchat.get_friends(update=True)
-    itchat.add_friend(userName=msg['FromUserName'], status=2)
-    itchat.add_friend(userName=msg['FromUserName'], status=3)
-    print 'sssssssss1'
-    print itchat.search_friends(userName=msg['FromUserName'])
-    print 'sssssssss2'
-    itchat.update_friend(userName=msg['FromUserName'])
-    print 'sssssssss3'
-    print itchat.search_friends(userName=msg['FromUserName'])
-    print 'sssssssss4'
+    # itchat.get_friends(update=True)
+    # itchat.add_friend(userName=msg['FromUserName'], status=2)
+    #itchat.add_friend(userName=msg['FromUserName'], status=3)
+    #print 'sssssssss1'
+
+
+    #print 'sssssssss2'
+    #itchat.update_friend(userName=msg['FromUserName'])
+    #print 'sssssssss3'
+    #print itchat.search_friends(userName=msg['FromUserName'])
+    #print 'sssssssss4'
     if msg['Text'] == 'streaming':
+        itchat.add_friend(userName=msg['FromUserName'], status=2)
+        itchat.add_friend(userName=msg['FromUserName'], status=3)
+        itchat.get_friends(update=True)
         userName = itchat.search_friends(userName=msg['FromUserName'])
-        userNamesToInvide = [userName]
-        # ↓↓↓↓↓ 以下是邀请到群里；需要之前就有 userNamesToInvide
-        roomName = itchat.search_chatrooms(name=u'Spark Streaming 交流群')[0]['UserName']
-        itchat.add_member_into_chatroom(roomName, userNamesToInvide, useInvitation=True)
-        # ↑↑↑↑↑ 以上是邀请到群里
+        print 'FromUserName = ' + msg['FromUserName']
+        if userName == None:
+            response = u'对不起，发送入群邀请暂未成功。\n几小时内机器人将自动重启并重试。'
+        else:
+            userNamesToInvide = [userName]
+            # ↓↓↓↓↓ 以下是邀请到群里；需要之前就有 userNamesToInvide
+            roomName = itchat.search_chatrooms(name=u'Spark Streaming 交流群')[0]['UserName']
+            itchat.add_member_into_chatroom(roomName, userNamesToInvide, useInvitation=True)
+            # ↑↑↑↑↑ 以上是邀请到群里
         response = u'Let\'s streaming!'
     else:
-        response = u'请回复如下关键字之一：\nstreaming'
+        response = u'请回复如下关键字：\nstreaming'
     return response
 
 
@@ -58,6 +74,7 @@ def text_reply(msg):
 def text_reply(msg):
     # itchat.send_msg(msg='hi', toUserName=msg['Text'][0]['UserName'])
     print '== msg_register(SYSTEM) begins ====='
+    printTime()
     print msg
     '''
     userName = msg['Text'][0]
@@ -74,6 +91,7 @@ def text_reply(msg):
     return u'Hi !!!'
 
 
+printTime()
 itchat.auto_login(hotReload=True)
 # rooms = itchat.get_chatrooms(update=True)
 # print rooms
