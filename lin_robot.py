b46acfbe8de6aff6db8deee01c55f3326cbe90f4
Liwei Lin <lwlin7@gmail.com>
2017-01-01 21:50:18 +0800

    updates

--- lin_robot.py
+++ lin_robot.py
@@ -40,11 +40,7 @@ def text_reply(msg):
     #print 'sssssssss3'
     #print itchat.search_friends(userName=msg['FromUserName'])
     #print 'sssssssss4'
-    if msg['Text'].strip().lower() == u'streaming' \
-            or msg['Text'].strip().lower() == u'coolplayspark' \
-            or msg['Text'].strip().lower() == u'coolplay spark' \
-            or msg['Text'].strip().lower() == u'cool play spark' \
-            or msg['Text'].strip().lower() == u'源码':
+    if msg['Text'].strip().lower() == u'streaming' or msg['Text'].strip().lower() == u'coolplayspark' or msg['Text'].strip().lower() == u'coolplay spark' or msg['Text'].strip().lower() == u'cool play spark' or msg['Text'].strip().lower() == u'源码':
         itchat.add_friend(userName=msg['FromUserName'], status=2)
         itchat.add_friend(userName=msg['FromUserName'], status=3)
         itchat.get_friends(update=True)
